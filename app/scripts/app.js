7b1af95066c4d169dff9e8ee90f455f69419dacd
shawnamckay <tamborine>
2018-07-09 14:37:58 -0600

    initial commit
    Contains basic structure of the app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sudokuAngularApp
+ * @description
+ * # sudokuAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sudokuAngularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sudokuAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sudokuAngularApp
+ */
+angular.module('sudokuAngularApp')
+  .controller('MainCtrl', function () {
+    this.numberList = [
+      '1','2','3','4','5','6','7','8','9'
+    ];
+  });
